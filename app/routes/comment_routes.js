45574fe90ca5be5f097c043d48c070a0af882936
Travis <travishersh0@gmail.com>
2023-02-13 15:35:03 -0500

    Update email to username and built comment routes

--- app/routes/comment_routes.js
+++ app/routes/comment_routes.js
@@ -27,4 +27,61 @@ const requireToken = passport.authenticate('bearer', { session: false })
 // instantiate a router (mini app that only handles routes)
 const router = express.Router()
 
+/////////////////////
+// ROUTES ///////////
+/////////////////////
+
+// CREATE
+router.post('/games/:gameId', requireToken, (req, res, next) => {
+    const gameId = req.params.gameId
+    // if we have errors, console.log this below 
+    req.body.comment.author = req.user.id
+    const theComment = req.body
+    Game.findById(gameId)
+        .then(handle404)
+        .then(game => {
+            game.comments.push(theComment)
+            return game.save()
+        })
+        .then(game => {
+            res.redirect(`/games/${game.id}`)
+        })
+        .then(() => res.sendStatus(204))
+        .catch(next)
+})
+
+// UPDATE
+router.patch('/games/:gameId/:commId', requireToken, (req, res, next) => {
+    const { gameId, commId } = req.params
+    Game.findById(gameId)
+        .then(handle404)
+        .then(game => {
+            const theComment = game.comments.id(commId)
+            // if we have an error, we look here
+            requireOwnership(req, game.comments.author)
+            theComment.title = req.body.title
+            theComment.note = req.body.note
+            game.markModified('comments')
+            game.save()
+        })
+        .then(() => res.sendStatus(204))
+        .catch(next)
+})
+
+// DELETE
+router.delete('/games/:gameId/:commId', requireToken, (req, res, next) => {
+    const { gameId, commId } = req.params
+    Game.findById(gameId)
+        .then(handle404)
+        .then(game => {
+            const theComment = game.comments.id(commId)
+            // if we have an error, we look here
+            requireOwnership(req, game.comments.author)
+            theComment.remove()
+            return game.save()
+        })
+        .then(() => res.sendStatus(204))
+        .catch(next)
+})
+
 module.exports = router 
