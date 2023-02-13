34a6de7aebe26ae974b07a3cb94a7436437ac0d6
danieljo <hyundanieljo@gmail.com>
2023-02-13 11:43:08 -0500

    setup comment subdoc and connected to game model

--- app/models/game.js
+++ app/models/game.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const commentSchema = require('./comment')
 
 const gameSchema = new mongoose.Schema(
 	{
@@ -18,6 +19,7 @@ const gameSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
+		comments: [commentSchema],
 		owner: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'User',
